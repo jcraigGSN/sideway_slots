e2d678b0f617f62ff7643b14e39f3f345f054596
Jim Craig <jcraig@gsn.com>
2014-10-13 10:03:10 -0700

    Initial Commit
    Hackweek October 2014 Sideways Slots Project

---
+++ Assets/scripts/mainScript.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class mainScript : MonoBehaviour
+{
+
+	// public class variables
+	public int mPublicSampleVar;
+
+	// protected class variables
+	protected int mProtectedSampleVar;
+
+	// private class methods
+	private int mPrivateSampleVar;
+
+
+	// Use this for initialization
+	void Start()
+	{
+		this.mPrivateSampleVar = 0;
+		this.mProtectedSampleVar = 0;
+		this.mPublicSampleVar = 0;
+	}
+	
+	// Update is called once per frame
+	void Update()
+	{
+		this.mPrivateSampleVar++;
+		this.mProtectedSampleVar++;
+		this.mPublicSampleVar++;
+	}
+}
