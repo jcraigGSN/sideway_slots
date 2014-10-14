745126863c27de834078f75184ac3a4f28123cfb
David Bontempo <dbontempo@gsn.com>
2014-10-14 10:20:33 -0700

    Sideways - Updated art. Added SPIN button with clicking.

---
+++ Assets/scripts/ButtonSpin.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ButtonSpin : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnMouseDown ()
+	{		
+		Debug.Log("   SPIN");
+		//transform.Rotate (Vector3.forward * -10);
+		iTween.RotateAdd (gameObject, iTween.Hash("z", -30, "time", 1)); 
+	}
+}
